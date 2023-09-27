3212808947e0319e1ba414c520dd1fb5c9dd360f
wyhong3103 <wyhong3103@gmail.com>
2023-09-27 20:41:49 +0800

    Add bot attributes to session model

--- src/models/session.entity.ts
+++ src/models/session.entity.ts
@@ -28,6 +28,12 @@ export class Session {
   @Column({ type: 'boolean', default: false })
   endedEarly: boolean;
 
+  @Column({ type: 'varchar', nullable: true })
+  botUrl: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  botAuthToken: string;
+
   @ManyToOne(() => Agent, (agent) => agent.sessions)
   agent: Agent;
 
